c6f07baa040ad990eaa2b9de8eb43454db596046
duebu-ananyapal <duebu.ananyapal@gmail.com>
2025-12-16 18:30:20 +0530

    added test and prod sp configuration

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dab_project_dec",
-    version="0.0.2",
-    description="This contains the code in the ./src directory",
-    author="Saurabh Pal",
     packages=find_packages(where="./src"),
-    package_dir={"":"./src"},
-    install_requires=["setuptools"]
+    package_dir={"":"./src"}
 )
