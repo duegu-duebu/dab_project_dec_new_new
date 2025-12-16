e0943c6c5e4c4eda6dd18db4cf78027639d7ed8c
duebu-ananyapal <duebu.ananyapal@gmail.com>
2025-12-16 11:36:11 +0530

    Edited the requirements_-dbc.txt file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project_dec",
-    version="0.0.1",
+    version="0.0.2",
     description="This contains the code in the ./src directory",
     author="Saurabh Pal",
     packages=find_packages(where="./src"),
